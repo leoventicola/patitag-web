038bc32799673950b0429615cc50e2782975484f
Leo Venticola <leandro.v@wyleex.com>
2024-05-04 15:40:34 -0300

    admin-lte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,75 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('adminlte::page')
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+{{-- Extend and customize the browser title --}}
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+@section('title')
+    {{ config('adminlte.title') }}
+    @hasSection('subtitle') | @yield('subtitle') @endif
+@stop
 
-        <!-- Styles -->
-        @livewireStyles
+{{-- Extend and customize the page content header --}}
 
-        <!-- Scripts -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+@section('content_header')
+    @hasSection('content_header_title')
+        <h1 class="text-muted">
+            @yield('content_header_title')
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
+            @hasSection('content_header_subtitle')
+                <small class="text-dark">
+                    <i class="fas fa-xs fa-angle-right text-muted"></i>
+                    @yield('content_header_subtitle')
+                </small>
             @endif
+        </h1>
+    @endif
+@stop
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+{{-- Rename section content to content_body --}}
 
-        @stack('modals')
+@section('content')
+    @yield('content_body')
+@stop
 
-        @livewireScripts
-    </body>
-</html>
+{{-- Create a common footer --}}
+
+@section('footer')
+    <div class="float-right">
+        Version: {{ config('app.version', '1.0.0') }}
+    </div>
+
+    <strong>
+        <a href="{{ config('app.company_url', '#') }}">
+            {{ config('app.company_name', 'My company') }}
+        </a>
+    </strong>
+@stop
+
+{{-- Add common Javascript/Jquery code --}}
+
+@push('js')
+<script>
+
+    $(document).ready(function() {
+        // Add your common script logic here...
+    });
+
+</script>
+@endpush
+
+{{-- Add common CSS customizations --}}
+
+@push('css')
+<style type="text/css">
+
+    {{-- You can add AdminLTE customizations here --}}
+    /*
+    .card-header {
+        border-bottom: none;
+    }
+    .card-title {
+        font-weight: 600;
+    }
+    */
+
+</style>
+@endpush
