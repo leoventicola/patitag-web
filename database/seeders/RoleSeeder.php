5c0a76b36ef9a660e91e2fb3b1f72cd4b34b90b8
Leo Venticola <lventicola@lavozdelinterior.com.ar>
2024-05-10 18:06:26 -0300

    roles and permission

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'User']);
+    }
+}
